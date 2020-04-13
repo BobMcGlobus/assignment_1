d135eddcd072af07402378d34fde6df31d0f8d36
Jonas <jonas.althoff@outlook.de>
2020-04-13 14:14:29 +0200

    anim v2

--- js/textAnim.js
+++ js/textAnim.js
@@ -5,7 +5,22 @@ function scrollAnim(){
 
     if ((screenSize/1.8) > textPosition){
 animText.classList.add('animActive');
-
+        setTimeout(anim2, 1000);
     }
 }
 window.addEventListener('scroll', scrollAnim);
+
+
+async function anim2(){
+    if (document.querySelector('.animActive')){
+        document.querySelector('.boxA').classList.add('animActive');
+        await Sleep(300);
+        document.querySelector('.boxB').classList.add('animActive');
+        await Sleep(300);
+        document.querySelector('.boxC').classList.add('animActive');
+
+    }
+}
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
